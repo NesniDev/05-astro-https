03240126e9c27b96b62c723f3391e2f095aec34e
Neider Nieto <nestnidev17@gmail.com>
2024-12-02 19:19:14 -0500

    APIRoute GET all

---
+++ src/pages/api/post/index.ts
@@ -0,0 +1,41 @@
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+export const prerender = false; 
+
+export const GET: APIRoute = async ({ params, request}) => {
+
+    const blogPost = await getCollection("blog")
+
+    const url = new URL(request.url)
+
+    const slug = url.searchParams.get("slug")
+ 
+    if(slug) {
+        const posts = blogPost.filter(p => p.slug === slug)
+        if(posts.length > 0){
+            return new Response(JSON.stringify(posts), {
+                status: 200,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+        }
+
+        return new Response(JSON.stringify({msg: `No se encontro el post con el path ${slug}`}), {
+            status: 404,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
+
+
+    return new Response(JSON.stringify(blogPost), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+}
